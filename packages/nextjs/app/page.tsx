c443dacb34cac41d7c8b4df8a53cdf92907dc3e6
Bhargav Kakadiya <kakadiyabhargav7@gmail.com>
2024-02-14 00:11:46 -0800

    add maps

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import dynamic from "next/dynamic";
 import Link from "next/link";
 import type { NextPage } from "next";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+const DynamicMap = dynamic(() => import("../components/Map"), {
+  loading: () => <p>Loading...</p>,
+});
 
 const Home: NextPage = () => {
   return (
@@ -28,7 +35,9 @@ const Home: NextPage = () => {
             </code>
           </p>
         </div>
-
+        <div className="w-2/3">
+          <DynamicMap />
+        </div>
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
             <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
---
+++ packages/nextjs/components/Map.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import React, { use } from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+const Map = () => {
+  const position = [51.505, -0.09]; // Example position: London
+
+  if (typeof window === "undefined") return;
+
+  return (
+    <MapContainer center={position} zoom={13} style={{ height: "400px", width: "100%" }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={position}>
+        <Popup>
+          A simple popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
