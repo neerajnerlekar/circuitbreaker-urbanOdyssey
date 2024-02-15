ac43245a1fe250efa2dca20c935162c88ec07f6c
Dharmik79 <ddholariya199@gmail.com>
2024-02-15 01:24:26 -0600

    added the form for the registration and set the validation using hook form

--- packages/nextjs/app/registration/page.tsx
+++ packages/nextjs/app/registration/page.tsx
@@ -7,20 +7,26 @@ export default function Home() {
   const methods = useForm();
   const onSubmit = data => console.log(data);
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4 py-8">
       <FormProvider {...methods}>
-        <form onSubmit={methods.handleSubmit(onSubmit)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <TextInput name="name" label="Name" type="text" />
-          <TextInput name="homeTown" label="HomeTown" type="text" />
-          <TextInput name="team" label="Team" type="text" />
-          <TextInput name="country" label="Country" type="text" />
-
-          <button
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            type="submit"
-          >
-            Submit
-          </button>
+        <form
+          onSubmit={methods.handleSubmit(onSubmit)}
+          className="max-w-lg mx-auto bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"
+        >
+          <div className="mb-4">
+            <TextInput name="name" label="Name" type="text" />
+            <TextInput name="homeTown" label="HomeTown" type="text" />
+            <TextInput name="team" label="Team" type="text" />
+            <TextInput name="country" label="Country" type="text" />
+          </div>
+          <div className="flex justify-center">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
         </form>
       </FormProvider>
     </div>
--- packages/nextjs/components/scaffold-eth/Input/TextInput.tsx
+++ packages/nextjs/components/scaffold-eth/Input/TextInput.tsx
@@ -9,12 +9,12 @@ const TextInput = ({ label, name, ...rest }) => {
   } = useFormContext();
 
   return (
-    <div className="mb-4">
+    <div className="mb-6">
       <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={name}>
         {label}
       </label>
       <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        className={`appearance-none border ${errors[name] ? 'border-red-500' : 'border-gray-300'} rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent`}
         id={name}
         {...register(name, { required: "This field is required" })}
         {...rest}
