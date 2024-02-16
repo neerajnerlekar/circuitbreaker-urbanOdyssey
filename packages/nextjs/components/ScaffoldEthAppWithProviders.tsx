2b45af6f861e7db3ebee0a27115e630f8e63ad56
Dharmik79 <ddholariya199@gmail.com>
2024-02-16 02:05:34 -0600

    removed the image from the centre

--- packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
+++ packages/nextjs/components/ScaffoldEthAppWithProviders.tsx
@@ -40,7 +40,7 @@ const ScaffoldEthApp = ({ children }: { children: React.ReactNode }) => {
 
   return (
     <>
-      <div className="flex flex-col min-h-screen bg-[url('../public/b1.png')] h-screen bg-cover bg-center">
+      <div className="flex flex-col min-h-screen bg-[url('../public/b1.png')] h-screen bg-cover ">
         <Header />
         <main className="relative flex flex-col flex-1 ">{children}</main>
         <Footer />
