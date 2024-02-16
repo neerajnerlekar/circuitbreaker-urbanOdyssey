848c2782455d20b5b170e987855c2fc5faf93123
Dharmik79 <ddholariya199@gmail.com>
2024-02-16 01:57:50 -0600

    removed the unncessary links formn teh header and set the icons

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -20,10 +20,18 @@ export const menuLinks: HeaderMenuLink[] = [
     href: "/",
   },
   {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
+    label:"Wallet",
+    href: "/wallet",
   },
+  {
+    label:"Registration Form",
+    href: "/registration",
+  },
+  {
+    label:"Location Form",
+    href: "/registration/location",
+  }
+ 
 ];
 
 export const HeaderMenuLinks = () => {
@@ -64,7 +72,7 @@ export const Header = () => {
   );
 
   return (
-    <div className="sticky lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
+    <div className=" lg:static top-0 navbar bg-base-100 min-h-0 flex-shrink-0 justify-between z-20 shadow-md shadow-secondary px-0 sm:px-2">
       <div className="navbar-start w-auto lg:w-1/2">
         <div className="lg:hidden dropdown" ref={burgerMenuRef}>
           <label
@@ -93,8 +101,8 @@ export const Header = () => {
             <Image alt="SE2 logo" className="cursor-pointer" fill src="/logo.svg" />
           </div>
           <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-ETH</span>
-            <span className="text-xs">Ethereum dev stack</span>
+            <span className="font-bold leading-tight">Urban Odyssey</span>
+       
           </div>
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
