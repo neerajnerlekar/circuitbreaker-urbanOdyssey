3e16f1e14c9226f894b08d7477231e380bbddd6b
Dharmik79 <ddholariya199@gmail.com>
2024-02-16 02:14:12 -0600

    chnage the button name

--- packages/nextjs/app/registration/page.tsx
+++ packages/nextjs/app/registration/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
               type="submit"
             >
-              Submit
+              Register
             </button>
           </div>
         </form>
