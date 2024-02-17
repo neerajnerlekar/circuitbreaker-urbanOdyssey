b50bd14c7713da83c8c62cca6e7de0558bf9f9f4
Dharmik79 <ddholariya199@gmail.com>
2024-02-16 23:46:44 -0600

    added the home page

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,12 +2,36 @@
 
 // import Link from "next/link";
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import { useAccount } from "wagmi";
 
 const Home: NextPage = () => {
+  const { isConnected } = useAccount();
+
+
+  const handleSubmit = () => {
+    console.log("clicked");
+    // Check for the status of the user info and allow the user to play the game and set the navigation accordingly
+  }
   return (
-    <div className="">
-      <h1 className="text-6xl text-white text-center pt-20">Welcome to</h1>
+    <div className="text-black text-center pt-20 ">
+  <h1 className="text-6xl">
+    Welcome to Urban Odyssey
+  </h1>
+
+  {isConnected ? (
+    <div className="mt-10 p-4  justify-center items-center h-screen">
+    
+      <button className="px-6 py-3 bg-blue-700 text-white rounded hover:bg-blue-800 transition duration-150 ease-in-out" onSubmit={handleSubmit}>
+        Play Game
+      </button>
     </div>
+  ) : (
+    <div className="mt-10 p-4  justify-center items-center h-screen ">
+      <p className="text-xl">Please connect the wallet to play the game</p>
+    </div>
+  )}
+</div>
   );
 };
 
