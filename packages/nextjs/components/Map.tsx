c5a67f63f4aef3a53e3a3086688807c87c1e0907
Bhargav Kakadiya <kakadiyabhargav7@gmail.com>
2024-02-16 22:31:34 -0800

    fix marker in  maps

--- packages/nextjs/components/Map.tsx
+++ packages/nextjs/components/Map.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+const iconMarker = L.icon({ iconUrl: "/marker-icon.png" });
 
 const Map = ({ position = [49.24539682005315, -123.1062698364258] }) => {
   if (typeof window === "undefined") return;
@@ -16,10 +19,8 @@ const Map = ({ position = [49.24539682005315, -123.1062698364258] }) => {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <Marker position={position} icon={iconMarker}>
+        <Popup>You are here</Popup>
       </Marker>
     </MapContainer>
   );
