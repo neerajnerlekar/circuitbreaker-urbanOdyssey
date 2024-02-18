7d403e4dd037496253fa5a8a491be8fcd7fa3c39
neerajnerlekar <neeraj.nerlekar@gmail.com>
2024-02-18 02:42:38 -0700

    add allplaces cards

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -4,16 +4,19 @@
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === 'true',
   },
   eslint: {
-    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
+    ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === 'true',
   },
   webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
+    config.resolve.fallback = { fs: false, net: false, tls: false }
+    config.externals.push('pino-pretty', 'lokijs', 'encoding')
+    return config
   },
-};
+  images: {
+    domains: ['ipfs.io'],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
