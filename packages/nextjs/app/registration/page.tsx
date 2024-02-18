40d0064e41298e31d24658dc700b8aa4fb0ca6d6
Bhargav Kakadiya <kakadiyabhargav7@gmail.com>
2024-02-17 16:29:22 -0800

    fix sign

--- packages/nextjs/app/registration/page.tsx
+++ packages/nextjs/app/registration/page.tsx
@@ -1,21 +1,12 @@
 "use client";
 
-import { Wallet } from "ethers";
-import { NFTStorage } from "nft.storage";
-import { useState } from "react";
+import { Wallet, hashMessage } from "ethers";
 import { FormProvider, useForm } from "react-hook-form";
 import TextInput from "~~/components/scaffold-eth/Input/TextInput";
 import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
-import keccak256 from "keccak256";
-async function signMessageWithEthers(privateKey, message) {
-  const wallet = new Wallet(privateKey);
-  const signature = await wallet.signMessage(message);
-  return signature;
-}
 
 export default function Home() {
   const methods = useForm();
-  const [selectedImage, setSelectedImage] = useState<any>(null); // Provide type any for selectedImage
 
   const handleImageChange = (e) => {
     if (e.target.files && e.target.files[0]) {
@@ -30,7 +21,7 @@ export default function Home() {
   const { writeAsync, isLoading } = useScaffoldContractWrite({
     contractName: "UrbanOdyssey",
     functionName: "registerPlayer",
-    args: ["", "", undefined, undefined, 1],
+    args: ["", "", "0x", "0x", 1],
     onBlockConfirmation: (txnReceipt) => {
       console.log("📦 Transaction blockHash", txnReceipt.blockHash);
     },
@@ -38,44 +29,28 @@ export default function Home() {
 
   const onSubmit = async (data: any) => {
     console.log("Register button clicked");
-    if (selectedImage) {
-      const client = new NFTStorage({
-        token: process.env.NEXT_PUBLIC_NFT_STORAGE_API_KEY || "",
-      });
-      client.storeBlob((selectedImage as any).imageFile).then(async (cid) => {
-        data.ipfsCID = cid;
-        const messageKey = await signMessageWithEthers(
-          process.env.NEXT_PUBLIC_PRIVATE_KEY,
-          `Register ${data.name} from ${data.homeTown} for team 1`
-        );
+    const msg = `Register`;
+    const msgHash = hashMessage(msg);
+    const privateKey = process.env.NEXT_PUBLIC_PRIVATE_KEY || "";
+    const wallet = new Wallet(privateKey);
+    console.log("Address corresponding to private key:", wallet.address);
+    const signature = await wallet.signMessage(arrayify(msgHash));
+    console.log({
+      msgHash: msgHash,
+      signature: signature,
+    });
 
-     
-        const signatureKey = await signMessageWithEthers(
-          process.env.NEXT_PUBLIC_PRIVATE_KEY,
-          "Hello from Signature For the team"
-        );
-console.log("Signature Key", signatureKey);
-        if (messageKey && signatureKey) {
-          writeAsync({
-            args: [data.name, data.homeTown, messageKey, signatureKey, 1],
-          });
-          console.log("Data submitted", data);
-        }
+    if (msgHash && signature) {
+      writeAsync({
+        args: [
+          data.name,
+          data.homeTown,
+          msgHash as `0x${string}`,
+          signature as `0x${string}`,
+          1,
+        ],
       });
-    } else {
-      const messageKey = keccak256(Buffer.from("Register "));
-      const signatureKey = await signMessageWithEthers(
-        process.env.NEXT_PUBLIC_PRIVATE_KEY,
-        "Hello from Signature For the team"
-      );
-      console.log("Message Key", Buffer.from(messageKey.buffer).toString());
-      console.log("Signature Key", signatureKey);
-      if (messageKey && signatureKey) {
-        writeAsync({
-          args: [data.name, data.homeTown, messageKey, signatureKey, 2],
-        });
-        console.log("Data submitted", data);
-      }
+      console.log("Data submitted", data);
     }
   };
 
@@ -84,8 +59,7 @@ console.log("Signature Key", signatureKey);
       <FormProvider {...methods}>
         <form
           onSubmit={methods.handleSubmit(onSubmit)}
-          className="max-w-lg mx-auto bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4"
-        >
+          className="max-w-lg mx-auto bg-white shadow-md rounded-lg px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
             {/* Existing TextInput fields */}
             <TextInput name="name" label="Name" type="text" />
@@ -114,43 +88,12 @@ console.log("Signature Key", signatureKey);
                 <span className="ml-2">TechnoMad</span>
               </label>
             </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 text-sm font-bold mb-2">
-                Upload Image
-              </label>
-              <input
-                type="file"
-                accept="image/*"
-                onChange={handleImageChange}
-                className="block w-full text-sm text-gray-500
-            file:mr-4 file:py-2 file:px-4
-            file:rounded-full file:border-0
-            file:text-sm file:font-semibold
-            file:bg-blue-50 file:text-blue-700
-            hover:file:bg-blue-100
-          "
-              />
-            </div>
-            {/* Image Preview */}
-            {selectedImage && (
-              <div className="mb-4">
-                <p className="block text-gray-700 text-sm font-bold mb-2">
-                  Preview
-                </p>
-                <img
-                  src={selectedImage.previewURL}
-                  alt="Preview"
-                  className="max-w-xs max-h-64"
-                />
-              </div>
-            )}
           </div>
           <div className="flex justify-center">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
               type="submit"
-              disabled={isLoading}
-            >
+              disabled={isLoading}>
               {isLoading ? (
                 <span className="loading loading-spinner loading-sm"></span>
               ) : (
@@ -162,4 +105,8 @@ console.log("Signature Key", signatureKey);
       </FormProvider>
     </div>
   );
+}
+
+function arrayify(msgHash: string): Uint8Array {
+  return new Uint8Array(Buffer.from(msgHash.slice(2), "hex"));
 }
