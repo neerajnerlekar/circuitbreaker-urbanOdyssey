eac1d4c7f591e21d82633c6015a32bec7cea4aa6
shyam-patel-kira <shyampkira@gmail.com>
2024-02-18 10:01:20 -0500

    update lint

--- packages/hardhat/contracts/lib/Structs.sol
+++ packages/hardhat/contracts/lib/Structs.sol
@@ -2,28 +2,28 @@
 pragma solidity ^0.8.20;
 
 library Structs {
-	struct Player {
-		uint256 verifiedPlaces;
-		uint256 registeredPlaces;
-		bool isVerified;
-		string name;
-		string hometown;
-		Faction faction;
-	}
+    struct Player {
+        uint256 verifiedPlaces;
+        uint256 registeredPlaces;
+        bool isVerified;
+        string name;
+        string hometown;
+        Faction faction;
+    }
 
-	struct Location {
-		address registeredBy;
-		uint8 level;
-		string placeName;
-		string placeType;
-		Faction faction;
-		int256 lat;
-		int256 long;
-		string ipfsURI;
-	}
+    struct Location {
+        address registeredBy;
+        uint8 level;
+        int256 lat;
+        int256 long;
+        string placeName;
+        string placeType;
+        string ipfsURI;
+        Faction faction;
+    }
 
-	enum Faction {
-		EcoGuardian,
-		TechnoMad
-	}
+    enum Faction {
+        EcoGuardian,
+        TechnoMad
+    }
 }
